e0a5aeb05d76366af3f4eb05290114bbf17a8281
Humeyranur <humeyranurturgut.ie@gmail.com>
2021-02-25 00:27:43 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> match = new MyDictionary<int>();
+            Console.WriteLine(match.Count);
+            match.Add(30);
+            match.Add(30);
+            match.Add(30);
+            match.Add(30);
+
+
+            Console.WriteLine(match.Count);
+         
+        }
+    }
+}
+
+
+
